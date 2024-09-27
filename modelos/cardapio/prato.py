f7d81b51060ee376c451cbfca2beabe4488dfaee
EduardoLovo <eduardo.llovo@gmail.com>
2024-09-27 17:50:05 -0300

    Criado class bebida e prato

--- modelos/cardapio/prato.py
+++ modelos/cardapio/prato.py
@@ -1,3 +1,6 @@
-class Prato:
+from modelos.cardapio.item_cardapio import ItemCardapio
+
+class Prato(ItemCardapio):
     def __init__(self, nome, preco, descricao ):
-        self._descricao = descricao
+        super().__init__(nome, preco)
+        self.descricao = descricao
